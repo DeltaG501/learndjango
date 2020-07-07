0c87a3d77392257a02647439363189d60980a44e
DeltaG501 <tom.te.luo+SignUp@gmail.com>
2020-07-07 22:53:26 +0800

    14 followed CoreySchafer on 07 Login & Logout

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == "POST":
@@ -8,8 +9,13 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'{username} 注册成功！')
-            return redirect('blog-home')
+            messages.success(request, f'{username} 注册成功！可以登录啦')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {"form":form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
