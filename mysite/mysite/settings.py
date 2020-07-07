2f102b4b38b4de2b98d1b2675bbe2d9ef5f23a4e
DeltaG501 <tom.te.luo+SignUp@gmail.com>
2020-07-07 14:07:07 +0800

    11 followed CoreySchafer on 03 templates

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
