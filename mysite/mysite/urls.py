a7cbf0a8770c47443a2bd28f226c474aac2c7cd3
DeltaG501 <tom.te.luo+SignUp@gmail.com>
2020-07-08 00:10:17 +0800

    15 followed CoreySchafer on 08 profiles and images for users

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,7 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('', include('blog.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
