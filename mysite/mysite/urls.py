d26e0e27eac6fe7a16ea6f94dd7ef80f5d71c566
DeltaG501 <tom.te.luo+SignUp@gmail.com>
2020-07-07 21:51:44 +0800

    13 followed CoreySchafer on 06 UserCreationForm, class meta, crispy_forms

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
     path('polls/', include('polls.urls')),
     path('blog/', include('blog.urls')),
     path('', include('blog.urls')),
