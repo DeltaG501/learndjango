758972a7a6973a1b76d00633e32df657c0a038fa
DeltaG501 <tom.te.luo+SignUp@gmail.com>
2020-07-08 21:57:03 +0800

    16 followed CoreySchafer on 09 updates profiles and images

--- mysite/users/signals.py
+++ mysite/users/signals.py
@@ -3,12 +3,13 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
 
+
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, *args, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
+
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, *args, **kwargs):
     instance.profile.save()
-
